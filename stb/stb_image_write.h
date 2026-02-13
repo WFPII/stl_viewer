4a3f6adc7bb46f70dce6501b0255b807dd036bb8
WFPII <will@virtlabs.dev>
2026-02-13 14:38:30 -0600

    Add files via upload

---
+++ stb/stb_image_write.h
@@ -0,0 +1,11 @@
+/* stb_image_write - v1.16 - public domain - http://nothings.org/stb
+ * Writes PNG files. See end for license.
+ * This is a STUB placeholder — download the real header from:
+ *   https://raw.githubusercontent.com/nothings/stb/master/stb_image_write.h
+ *
+ * The setup.sh script will download this automatically.
+ */
+#ifndef STB_IMAGE_WRITE_PLACEHOLDER
+#define STB_IMAGE_WRITE_PLACEHOLDER
+#error "Please run ./setup.sh to download the real stb_image_write.h"
+#endif
